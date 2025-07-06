e215a1cd69d805d3ed29ed532689cce46c07c0d4
Himanshu2-8 <pragyan.himanshu2004@gmail.com>
2025-07-06 15:27:07 +0530

    routing to articles with id done

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -2,24 +2,29 @@ import { currentUser } from "@clerk/nextjs/server"
 import prisma from "@/lib/prisma";
 
 const layout = async ({children}:{children:React.ReactNode}) => {
-    const user=await currentUser()
-    if(!user) return null;
-
-    const loggedInUser=await prisma.user.findUnique({
-        where:{
-            clerkUserId:user.id
-        }
-    })
-    if(!loggedInUser){
-        await prisma.user.create({
-            data:{
-                clerkUserId:user.id,
-                name:user.fullName || "",
-                email:user.emailAddresses[0].emailAddress,
-                imageUrl:user.imageUrl
+    const user = await currentUser()
+    
+    if (user) {
+        const loggedInUser = await prisma.user.findUnique({
+            where: {
+                clerkUserId: user.id
             }
         })
+        
+        if (!loggedInUser && user.emailAddresses[0]?.emailAddress) {
+            await prisma.user.upsert({
+                where: { clerkUserId: user.id },
+                update: {},
+                create: {
+                    clerkUserId: user.id,
+                    name: user.fullName || "",
+                    email: user.emailAddresses[0].emailAddress,
+                    imageUrl: user.imageUrl
+                }
+            })
+        }
     }
+
     return (
         <div>
             {children}
